90c7694f89786156216e66190ec8341c2791c319
Your name <youremail@example.com>
2011-01-20 18:33:05 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MingApp::Application.config.secret_token = '42edf5871ae374123d7a8d9f8aa385fbd47cf8b3b12dff96d3b511ab90e6005821c966bf172fe4e37d867be1eed1a277c433ea9e2f3d8620cdcf2ca8a48f7979'
